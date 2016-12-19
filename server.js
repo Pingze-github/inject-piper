b6df847c4ec2cfa49419517db47f38162b3020bb
pingze <wang719695@gmail.com>
2016-12-19 16:19:42 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+
+/**IMPORT**/
+var express = require('express');
+var url = require('url');
+var inject_pipe = require('./inject-pipe.js');
+
+/** SET **/
+var app = express();
+
+/**FUNCTION**/
+function sendViewMiddleware(req, res, next) { //send .html
+    res.sendHtml = function(html) {
+        return res.sendFile(__dirname + "/" + html);
+    }
+    next();
+}
+app.use(sendViewMiddleware);
+
+/**GET**/
+app.get('/', function (req, res) { //主页
+    console.log("[SERVER][GET] /");
+    res.sendHtml('index.html');
+});
+
+app.get('/pipe', function (req, res) { //pipe
+    console.log("[SERVER][GET] /pipe");
+    var params = url.parse(req.url,true).query;
+    console.log(params);
+    var href = params['href']
+    inject_pipe.pipe(href,function(html){
+        res.send(html);
+    });
+});
+
+/**启动服务器**/
+var server = app.listen(10000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Sever Start @ http://%s:%s", host, port)
+}); //于指定端口启动服务器
+
+
+
